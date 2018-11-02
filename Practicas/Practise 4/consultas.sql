385e3d4248d848712a8cd1bd4801bbf0f5b0482b
Francisco Javier Blazquez Martinez <fcojavibm1@gmail.com>
2018-11-02 18:37:36 +0100

    Practise 4 finished

--- Practicas/Practise 4/consultas.sql
+++ Practicas/Practise 4/consultas.sql
@@ -1,172 +1,175 @@
--- FRANCISCO JAVIER BL¡ZQUEZ MARTÕNEZ  ~  frblazqu@ucm.es
--- MANUEL ORTEGA SALVADOR              ~  manuor01@ucm.es
+-- FRANCISCO JAVIER BL√ÅZQUEZ MART√çNEZ ~ frblazqu@ucm.es
+-- MANUEL ORTEGA SALVADOR ~ manuor01@ucm.es
 --
---   Doble grado IngenierÌa inform·tica - Matem·ticas
---	 Universidad Complutense de Madrid.
+-- Doble grado Ingenier√≠a inform√°tica - Matem√°ticas
+--	Universidad Complutense de Madrid.
 --
 -- Comentarios:
+-- Todo el c√≥digo de creaci√≥n de tablas y de inserci√≥n de datos es √∫nicamente
+-- para probar las consultas en el entorno DES (http://des.sourceforge.net/)
+-- y obviamente no es parte de las consultas que se deben entregar en la pr√°ctica.
 
+
+-- 0.- Eliminaci√≥n de tablas anteriores y configuraci√≥n para SQL:
+-- 0.1.- Eliminar las relaciones que tuviera la bd
 /abolish
-/multiline on
+-- 0.2.- Para procesar solo instrucciones SQL
 /sql
+-- 0.3.- Para permitir la inserci√≥n multilinea de instrucciones
+/multiline on
 
--- 1.- CreaciÛn de tablas:
-Create table Empleados
+
+-- 1. - Creaci√≥n de tablas
+Create table Empleados 
 (
-    Nombre string not null, 
+	Nombre char(50), 
+  	DNI Char(9), 
+  	Sueldo Number(6,2),
+  	primary key(DNI),
+  	check (Sueldo between 735.9 and 5000.0)
+);
+Create table "C√≥digos postales"
+(
+    "C√≥digo postal" Char(5),
+    Poblaci√≥n Char(50),
+    Provincia Char(50),
+    primary key("C√≥digo postal")
+);
+Create table Domicilios 
+(
+	DNI Char(9),
+  	Calle Char(50),
+  	"C√≥digo postal" Char(5),
+  	primary key (DNI, Calle, "C√≥digo postal"),
+  	foreign key (DNI) references Empleados (DNI),
+  	foreign key ("C√≥digo postal") references "C√≥digos postales" ("C√≥digo postal")
+);  	
+Create table Tel√©fonos
+(
     DNI Char(9), 
-    Sueldo float, 
-    primary key (DNI),
-    check(Sueldo between 736.0 and 5000.0)
-);
-Create table "CÛdigos postales"
-(
-    "CÛdigo postal" Char(5),
-    PoblaciÛn string,
-    Provincia string,
-    primary key("CÛdigo postal")
-);
-Create table Domicilios
-(
-    DNI Char(9), 
-    Calle string, 
-    "CÛdigo postal" Char(5),
-    primary key (DNI, Calle, "CÛdigo postal"),
-    foreign key (DNI) references Empleados(DNI),
-    foreign key ("CÛdigo postal") references "CÛdigos postales"("CÛdigo postal")
-);
-Create table TelÈfonos
-(
-    DNI Char(9), 
-    TelÈfono Char(9),
-    primary key (DNI, TelÈfono),
-    foreign key (DNI) references Empleados(DNI)
+    Tel√©fono Char(9),
+    primary key (DNI, Tel√©fono),
+    foreign key (DNI) references Empleados(DNI) 
 );
 
--- 2.- InserciÛn de elementos:
-insert into Empleados values ('Antonio Arjona', '12345678A', 5000.0);
-insert into Empleados values ('Carlota Cerezo', '12345678C', 1000.0);
-insert into Empleados values ('Laura LÛpez', '12345678L', 1500.0);
-insert into Empleados values ('Pedro PÈrez' ,'12345678P' ,2000.0);
+-- 2. Inserci√≥n
+insert into Empleados(Nombre,DNI,Sueldo) values ('Antonio Arjona', '12345678A', 5000.0);
+insert into Empleados(Nombre,DNI,Sueldo) values ('Carlota Cerezo', '12345678C', 1000.0);
+insert into Empleados(Nombre,DNI,Sueldo) values ('Laura L√≥pez', '12345678L', 1500.0);
+insert into Empleados(Nombre,DNI,Sueldo) values ('Pedro P√©rez' ,'12345678P' ,2000.0);
 
-insert into TelÈfonos values ('12345678C', '611111111');
-insert into TelÈfonos values ('12345678C', '931111111');
-insert into TelÈfonos values ('12345678L', '913333333');
-insert into TelÈfonos values ('12345678P', '913333333');
-insert into TelÈfonos values ('12345678P', '644444444');
+insert into Tel√©fonos(DNI, Tel√©fono) values ('12345678C', '611111111');
+insert into Tel√©fonos(DNI, Tel√©fono) values ('12345678C', '931111111');
+insert into Tel√©fonos(DNI, Tel√©fono) values ('12345678L', '913333333');
+insert into Tel√©fonos(DNI, Tel√©fono) values ('12345678P', '913333333');
+insert into Tel√©fonos(DNI, Tel√©fono) values ('12345678P', '644444444');
 
-insert into "CÛdigos postales" values ('08050', 'Parets', 'Barcelona');
-insert into "CÛdigos postales" values ('14200', 'PeÒarroya', 'CÛrdoba');
-insert into "CÛdigos postales" values ('14900', 'Lucena', 'CÛrdoba');
-insert into "CÛdigos postales" values ('28040', 'Madrid', 'Madrid');
-insert into "CÛdigos postales" values ('50008', 'Zaragoza', 'Zaragoza');
-insert into "CÛdigos postales" values ('28004', 'Arganda', 'Madrid');
+insert into "C√≥digos postales"("C√≥digo postal", Poblaci√≥n, Provincia) values ('08050', 'Parets', 'Barcelona');
+insert into "C√≥digos postales"("C√≥digo postal", Poblaci√≥n, Provincia) values ('14200', 'Pe√±arroya', 'C√≥rdoba');
+insert into "C√≥digos postales"("C√≥digo postal", Poblaci√≥n, Provincia) values ('14900', 'Lucena', 'C√≥rdoba');
+insert into "C√≥digos postales"("C√≥digo postal", Poblaci√≥n, Provincia) values ('28040', 'Madrid', 'Madrid');
+insert into "C√≥digos postales"("C√≥digo postal", Poblaci√≥n, Provincia) values ('50008', 'Zaragoza', 'Zaragoza');
+insert into "C√≥digos postales"("C√≥digo postal", Poblaci√≥n, Provincia) values ('28004', 'Arganda', 'Madrid');
 
-insert into Domicilios values ('12345678A', 'Avda. Complutense', '28040');
-insert into Domicilios values ('12345678A', 'C·ntaro', '28004');
-insert into Domicilios values ('12345678P', 'Diamante', '14200');
-insert into Domicilios values ('12345678P', 'CarbÛn', '14900');
-insert into Domicilios values ('12345678L', 'Diamante', '14200');
+insert into Domicilios(DNI,Calle,"C√≥digo postal") values ('12345678A', 'Avda. Complutense', '28040');
+insert into Domicilios(DNI,Calle,"C√≥digo postal") values ('12345678A', 'C√°ntaro', '28004');
+insert into Domicilios(DNI,Calle,"C√≥digo postal") values ('12345678P', 'Diamante', '14200');
+insert into Domicilios(DNI,Calle,"C√≥digo postal") values ('12345678P', 'Carb√≥n', '14900');
+insert into Domicilios(DNI,Calle,"C√≥digo postal") values ('12345678L', 'Diamante', '14200');
 
--- 3.- Consultas:
 
--- 3.1.- Listado de los empleados con domicilio ordenados por CÛdigo postal y Nombre:
-create view vista1 as
+-- 3. -Consultas
+-- 3.1.- Listado de los empleados con domicilio ordenados por C√≥digo postal y Nombre
+create view vista1 as 
+  select Nombre, Calle, "C√≥digo postal"
+  from Empleados natural inner join Domicilios
+  order by "C√≥digo postal", Nombre;
 
-select distinct Nombre,Calle,"CÛdigo postal" 
-from Empleados natural join Domicilios
-order by "CÛdigo postal",Nombre;
-
--- 3.2.- Empleados que tengan telÈfono ordenados por nombre:
+-- 3.2.- Empleados que tengan tel√©fono ordenados por nombre:
 create view vista2 as
 
-select Nombre,Domicilios.DNI,Calle,"CÛdigo postal",TelÈfono
-from (Empleados natural join TelÈfonos) natural left outer join Domicilios 
+select Nombre,Domicilios.DNI,Calle,"C√≥digo postal",Tel√©fono
+from (Empleados natural join Tel√©fonos) natural left outer join Domicilios 
 order by Nombre;
 
--- 3.3.- Listado de todos los empleados ordenados por nombre (Esquema de valores nulos fijo):
-create view vista3 as 
-
-select Nombre,Empleados.DNI,Calle,"CÛdigo postal",TelÈfono
-from (Empleados natural left outer join TelÈfonos) left outer join Domicilios
-order by Nombre;
+-- 3.3.- Listado de todos los empleados ordenados por nombre, tanto los empleados que tengan tel√©fono
+--       como los que no
+create view vista3 as
+	select Nombre, DNI, Calle, "C√≥digo postal", Tel√©fono
+	from (Empleados natural left outer join Tel√©fonos) natural left outer join Domicilios 
+	order by Nombre;
 
 -- 3.4.- Listado de todos los empleados ordenados por nombre (Esquema de valores nulos fijo):
 create view vista4 as
 
-select Nombre,DNI,Calle,PoblaciÛn,Provincia,"CÛdigo postal"
-from (Empleados natural left outer join Domicilios) natural left outer join "CÛdigos postales"
+select Nombre,DNI,Calle,Poblaci√≥n,Provincia,"C√≥digo postal"
+from (Empleados natural left outer join Domicilios) natural left outer join "C√≥digos postales"
 order by Nombre;
 
--- 3.5.- Listado de todos los empleados ordenados por nombre (Esquema de valores nulos fijo):
+-- 3.5.- Listado de todos los empleados ordenados por nombre
 create view vista5 as
+	select Nombre, DNI, Calle, Poblaci√≥n, Provincia, "C√≥digo postal", Tel√©fono
+	from (Empleados natural left outer join Tel√©fonos) natural left outer join Domicilios natural left outer 		  join "C√≥digos postales"
+	order by Nombre;
 
-select Nombre,DNI,Calle,PoblaciÛn,Provincia,"CÛdigo postal",TelÈfono
-from ((Empleados natural left outer join Domicilios) natural left outer join "CÛdigos postales")
-       natural left outer join TelÈfonos
-order by Nombre;
-
--- 3.6.- Incrementa los salarios de todos los empleados un 10% (acotado por 1900Ä):
+-- 3.6.- Incrementa los salarios de todos los empleados un 10% (acotado por 1900¬Ä):
 update Empleados set Sueldo=1.1*Sueldo where 1.1*Sueldo<=1900.0;
 
 create view vista6 as select * from Empleados;
 
--- 3.7.- Deshacer la operaciÛn anterior (NO LA DESHACE SIEMPRE!!):
-update Empleados set Sueldo=Sueldo/(1.1) where Sueldo<=1900.0;
+-- 3.7.- Deshaz la operaci√≥n anterior con otra instrucci√≥n UPDATE
+update Empleados set Sueldo = Sueldo/1.1
+where Sueldo <= 1900.0;
 
-create view vista7 as select * from Empleados;
-
--- 3.8.- Probar los apartados 6 y 7 con una acotaciÛn de 1600Ä, øQuÈ sucede?:
+-- 3.8.- Probar los apartados 6 y 7 con una acotaci√≥n de 1600¬Ä, ¬øQu√© sucede?:
 
 -- update Empleados set Sueldo=1.1*Sueldo where 1.1*Sueldo<=1600.0;
 -- update Empleados set Sueldo=Sueldo/(1.1) where Sueldo<=1600.0;
 -- 
 -- create view vista8 as select * from Empleados;
 
--- Lo que sucede es que mientras que Carlota est· algo triste por volver a su salario
--- original antes de que la subida y Antonio y Pedro est·n indiferentes por completo
--- con el mismo salario de siempre, Laura est· rabiando porque no solo nunca viÛ una
--- subida sino que viÛ una bajada (ahora cobra unos 140Ä menos).
+-- Lo que sucede es que mientras que Carlota est√° algo triste por volver a su salario
+-- original antes de que la subida y Antonio y Pedro est√°n indiferentes por completo
+-- con el mismo salario de siempre, Laura est√°¬° rabiando porque no solo nunca vi√≥ una
+-- subida sino que vi√≥ una bajada (ahora cobra unos 140eur menos).
 --
--- El problema viene causado porque una operaciÛn no es inversa a la anterior, esto es,
--- hay rangos que no experimentan subida pero que sÌ que experimentan bajada de salarios.
+-- El problema viene causado porque una operaci√≥n no es inversa a la anterior, esto es,
+-- hay rangos que no experimentan subida pero que s√≠ que experimentan bajada de salarios.
 
--- 3.9.- Listado con n∫ de empleados, salario mÌnimo, m·ximo y medio.
-create view vista9 as
+-- 3.9.- Listado que muestre n√∫mero total de empleados, el sueldo m√≠nimo, el m√°ximo y el medio
+create view vista9 as	
+	select count(*) as Empleados, min(Sueldo) as "Sueldo m√≠nimo", max(Sueldo) as "Sueldo m√°ximo", 			 	   avg(Sueldo) as "Sueldo medio"
+	from Empleados;
 
-select count(*),min(Sueldo),max(Sueldo),avg(Sueldo)
-from Empleados;
-
--- 3.10.- Suledo medio por poblaciÛn y n˙mero de empleados, ordenado por poblaciÛn:
+-- 3.10.- Listado que muestre sueldo medio por poblaci√≥n y n√∫mero de empleados, ordenado por
+--		  poblaci√≥n
 create view vista10 as
-	
-select count(*),avg(Sueldo),PoblaciÛn 
+    
+select count(*),avg(Sueldo),Poblaci√≥n 
 from (Empleados natural left outer join Domicilios) 
-	            natural left outer join "CÛdigos postales"
-group by PoblaciÛn
-order by PoblaciÛn;
+                natural left outer join "C√≥digos postales"
+group by Poblaci√≥n
+order by Poblaci√≥n;
 
--- 3.11.- Obtener los empleados con m·s de un telÈfono ordenados por nombre:
+-- 3.11.- Obtener los empleados que tengan m√°s de un tel√©fono, indicando Nombre, DNI y Tel√©fono,
+--		  ordenados por su nombre
 create view vista11a as
 
-select distinct Nombre,Empleados.DNI,t1.TelÈfono
-from Empleados, TelÈfonos t1, TelÈfonos t2
-where Empleados.DNI=t1.DNI and Empleados.DNI=t2.DNI and t1.TelÈfono != t2.TelÈfono
+select distinct Nombre,Empleados.DNI,t1.Tel√©fono
+from Empleados, Tel√©fonos t1, Tel√©fonos t2
+where Empleados.DNI=t1.DNI and Empleados.DNI=t2.DNI and t1.Tel√©fono != t2.Tel√©fono
 order by Nombre;
 
-  -- Eliminame si quieres, pensaba que quedaba mejor, m·s corto.
 create view vista11b as
-
-select * from 
-
-(select Nombre,DNI 
-from Empleados Emp
-where (select count(TelÈfono) from TelÈfonos where TelÈfonos.DNI=Emp.DNI)>1)
-
-natural join TelÈfonos
-order by Nombre;
+	with Cantidad_tel√©fonos_emp(DNI, numTel√©fonos) as
+		select DNI, count(*) numTel√©fonos
+		from Empleados natural inner join Tel√©fonos
+		group by DNI
+	select Nombre, DNI, Tel√©fono
+	from (Empleados natural inner join Cantidad_tel√©fonos_emp) natural inner join Tel√©fonos
+	where numTel√©fonos > 1
+	order by Nombre;
 
 
 /multiline off
 /datalog
-
